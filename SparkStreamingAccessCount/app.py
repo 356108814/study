32902fe8ff5752445d133511acee7713c22beab2
root <root@localhost.localdomain>
2016-08-15 19:54:07 +0800

    app

--- SparkStreamingAccessCount/app.py
+++ SparkStreamingAccessCount/app.py
@@ -1,6 +1,8 @@
 # park Application - execute with spark-submit：
 # spark-submit app.py
 ## Imports
+import time
+import re
 from pyspark import SparkConf, SparkContext
 
 ## Module Constants
@@ -8,10 +10,26 @@ APP_NAME = "SparkStreamingAccessCount"
 
 ## Closure Functions
 
+def parse(line):
+    reg = re.compile('(?P<ip>.*) - - (?P<date>.*) "(?P<method>.*) (?P<uri>.*) (?P<version>.*)" (?P<status>.*) (?P<content_length>.*) "(?P<referer>.*)" "(?P<user_agent>.*)" (?P<request_time>.*)ms')
+    match = reg.match(line)
+    if not match:
+        print(line)
+    groupdict = match.groupdict()
+    groupdict['uri'] = groupdict['uri'].split('?')[0]    # 去掉参数
+    return groupdict
+
+
 ## Main functionality
 
 def main(sc):
-    pass
+    start_time = time.time()
+    textFile = sc.textFile('./data/access.log')
+    rdd = textFile.map(parse)    # PyrhonRDD对象
+    result = rdd.collect()    # 解析结果数组，每个元素为parse返回值
+    with open('dictdata', 'w+') as f:
+        f.writelines(str(result))
+        print('处理完成:%s' % (time.time() - start_time))
 
 if __name__ == "__main__":
     # Configure Spark
