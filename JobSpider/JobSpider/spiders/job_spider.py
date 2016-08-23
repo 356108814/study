da2f9296d04eccf644a3d037b6eab5084f337c8a
忧里 修斯 <769435570@qq.com>
2016-08-23 23:07:26 +0800

    add parse

--- JobSpider/JobSpider/spiders/job_spider.py
+++ JobSpider/JobSpider/spiders/job_spider.py
@@ -23,7 +23,32 @@ class LagouJobSpider(scrapy.Spider):
         #     f.write(response.body)
 
         item = JobspiderItem()
-        item.name = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
-        item.salary = response.xpath('//td[@id="item_name"]/text()').extract()
-        # item['description'] = response.xpath('//td[@id="item_description"]/text()').extract()
+        job_detail = response.xpath('//dl[@class="job_detail"]')[0]
+        title = job_detail.xpath('./dt/h1')[0].get('title', '')    # get获取属性
+        job_request = response.xpath('//dd[@class="job_request"]')[0]
+        spans = job_request.xpath('./p')[0].xpath('./span')    # ./表示从当前节点开始查找
+        salary = spans[0].text
+        location = spans[1].text
+
+        job_bt = response.xpath('//dd[@class="job_bt"]')[0]
+        descs = []
+        description = job_bt.xpath('./p')
+        # 岗位描述和要求以<p><br></p>隔开，text为None
+        is_desc = True
+        requirements = []
+        for d in description:
+            text = d.text
+            if text:
+                if is_desc:
+                    descs.append(text)
+                else:
+                    if text != u'岗位要求：':
+                        requirements.append(text)
+            else:
+                is_desc = False
+        print title
+        print salary
+        print location
+        print descs
+        print requirements
         return item
